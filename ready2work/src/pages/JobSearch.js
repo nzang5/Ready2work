6c189be25bd24667454c1860161e93d13dc70c9e
Nash Zangio <nash.c.zangio@gmail.com>
2022-09-06 21:45:58 +0200

    api console.log

--- ready2work/src/pages/JobSearch.js
+++ ready2work/src/pages/JobSearch.js
@@ -1,15 +1,39 @@
 import React from "react";
+import axios from 'axios';
+import { useState, useEffect } from "react";
+
+const options = {
+    method: 'GET',
+    url: 'https://arbeitnow-free-job-board.p.rapidapi.com/api/job-board-api',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-RapidAPI-Key': '8ce0e4d62dmsh944df266a00df01p126174jsn0198eac77e75',
+      'X-RapidAPI-Host': 'arbeitnow-free-job-board.p.rapidapi.com'
+    }
+  };
 
 
 function JobSearch(){
 
-
+    const [ jobs, setJobs ] = useState();
+    
+    useEffect(() => {
+      
+        axios.request(options)
+        .then(function (response) {console.log(response.data)
+        setJobs(response.data);})
+        .catch(function (error) {
+            console.error(error);
+        },[]);
+ 
+    })
 
     return(
         <div>
             <h1>Job Search page</h1>
-        </div>
+      </div>
     )
+    
 }
 
 export default JobSearch;
