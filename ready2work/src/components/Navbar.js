fa1168b1b8df787cfa3807f937b4c0a80a57ed58
Nash Zangio <nash.c.zangio@gmail.com>
2022-09-06 19:25:49 +0200

    basic navbar

---
+++ ready2work/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Navbar(){
+
+
+    return(
+        <nav>
+            <h2>Home</h2>
+            <h2>Sign-in</h2>
+            <h2>Sign-up</h2>
+        </nav>
+    )
+}
+
+export default Navbar;
