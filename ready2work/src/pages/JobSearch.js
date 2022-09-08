5ac7a538f74501fd0800fff232a5dfd11230a7df
Nash Zangio <nash.c.zangio@gmail.com>
2022-09-08 15:02:41 +0200

    ext api

--- ready2work/src/pages/JobSearch.js
+++ ready2work/src/pages/JobSearch.js
@@ -1,57 +1,57 @@
 import React from "react";
-import axios from 'axios';
+import Axios from 'axios';
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 const options = {
   method: 'GET',
-  url: 'https://linkupjobsearch-linkup-job-search-v1.p.rapidapi.com/developers/v-1/search-handler.js',
-  params: {
-    api_key: '{api_key}',
-    embedded_search_key: '{search_key}',
-    orig_ip: '{ip_addr}',
-    keyword: '{keyword}',
-    location: '{location}',
-    distance: '{radius}'
-  },
+  url: 'https://arbeitnow-free-job-board.p.rapidapi.com/api/job-board-api',
   headers: {
-    Authorization: 'undefined',
+    'Content-Type': 'application/json',
     'X-RapidAPI-Key': '8ce0e4d62dmsh944df266a00df01p126174jsn0198eac77e75',
-    'X-RapidAPI-Host': 'linkupjobsearch-linkup-job-search-v1.p.rapidapi.com'
+    'X-RapidAPI-Host': 'arbeitnow-free-job-board.p.rapidapi.com'
   }
 };
 
 
 function JobSearch(){
-
+    
     const [ jobs, setJobs ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
     
     useEffect(() => {
-      
-        axios.request(options)
-        .then(response=> 
-        {console.log(response.data)
-        setJobs(response.data);})
-        .catch(function (error) {
-            console.error(error);
-        },[]);
- 
-    })
+      Axios.request(options)
+      .then((response) => {
+        console.log(response.data.data);
+      const apiData = response.data.data;
+      setJobs(apiData);
+      setLoading(false)
+      })
+      .catch((err) => console.log(err));
+    }, []);
     
-
+if(loading === true){
+  return <p>Loading...</p>
+}
         return(
             <div>
-                {jobs.map((jobs, index) => {
-        return (
-          <div key={index}>
-            <h2>name: {jobs.company_name}</h2>
-            <h2>country: {jobs.title}</h2>
+            <h3>List of jobs</h3>
 
+                {jobs.map((job => {
+        return (
+          <div>
+            <h2> {job.title}</h2>
+            <h3> {job.company_name}</h3>
+            <p> {job.description}</p>
+             <Link to="/{job.url}">Apply here</Link>
             <hr />
           </div>
         );
-      })}
+      }))
+      }
 
+     
 
 
       </div>
