9070167fbd30222d8bdde2b40b01795ef880ad14
Nash Zangio <nash.c.zangio@gmail.com>
2022-09-14 11:33:33 +0200

    signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,12 +1,24 @@
 // src/pages/SignupPage.js
 
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Googlebutton from '../components/Googlebutton'
 
 
-const API_URL = process.env.REACT_APP_API_URL||"https://dark-erin-panther-garb.cyclic.app" ;
+// 1. Below is the Material UI template for the "Modal" that pops up the login page)
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const API_URL = process.env.REACT_APP_API_URL||"https://localhost:5005" ;
+
+const theme = createTheme();
+
 
 
 
@@ -14,6 +26,7 @@ function Signup(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+      // eslint-disable-next-line 
   const [errorMessage, setErrorMessage] = useState(undefined);
 
   const navigate = useNavigate();
@@ -47,13 +60,25 @@ function Signup(props) {
 
   
   return (
-    <div className="SignupPage">
-    
+    <ThemeProvider theme={theme}>
+      <div component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}>
       <h1>Sign Up</h1>
 
-      <form onSubmit={handleSignupSubmit}>
+      <Box component="form" onSubmit={handleSignupSubmit} noValidate sx={{ mt: 1 }}>
         <label>Email:</label>
-        <input 
+        <TextField 
+        margin="normal"
+              required
+              fullWidth
+          label="Email Address"
           type="email"
           name="email"
           value={email}
@@ -61,7 +86,11 @@ function Signup(props) {
         />
 
         <label>Password:</label>
-        <input 
+        <TextField 
+        margin="normal"
+              required
+              fullWidth
+          label="Password"
           type="password"
           name="password"
           value={password}
@@ -69,25 +98,40 @@ function Signup(props) {
         />
 
         <label>Name:</label>
-        <input 
+        <TextField 
+        margin="normal"
+              required
+              fullWidth
+          label="Name"    
           type="text"
           name="name"
           value={name}
           onChange={handleName}
         />
 
-        <button type="submit">Sign Up</button>
-      </form>
-      
-      
+        <Button type="submit" 
+              fullWidth
+              variant ="contained"
+              sx={{ mt: 3, mb: 2 }}>
+              Sign Up
+              </Button>
 
-      { errorMessage && <p className="error-message">{errorMessage}</p> }
-
-      <Googlebutton />
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
-    </div>
+              <Grid container>
+              <Grid item>
+                <Link href="/" variant="body2">
+                  <p>Already have account? Login!</p>
+                </Link>
+              </Grid>
+            </Grid>
+              </Box>
+              </Box>
+              </div>
+              </ThemeProvider>
   )
-}
+        }
+
+     
+  
+
 
 export default Signup;
