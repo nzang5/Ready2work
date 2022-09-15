14487f3f59185073293dedacc74a4361e5a04433
Lorena Garcia <lorena_65_11@hotmail.com>
2022-09-15 12:49:20 +0200

    css about and job search

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import pic from "../components/Screenshot_20220805-095639_Instagram.jpg";
 import foto from "../components/Screenshot 2022-09-13 at 20.47.43.png";
+import  GitHub  from './GitHub.png';
+import LinkedIn from './linkedin_icon.png'
 
 const About = () => {
   return (
@@ -12,15 +14,7 @@ const About = () => {
             Nash Zangio
         </h1>
         <div className="descriptioncontainer">
-            <p>
-            
-                adasdasdasdasdasda
-                asdasdasrtdhsfdghsdasadasdasdas
-                testtesttest
-            </p>
-            <p>adasdasdasdasdasdaasdasdas
-            
-            </p>
+            <p>adasdasdasdasdasdaasdasdas</p>
          </div>
         
 
@@ -30,13 +24,10 @@ const About = () => {
         <img alt='Lorena' className='lorenaPic' src={pic}></img>
         <h1>Lorena Garcia</h1>
          <div className="descriptioncontainer">
-            <p>
-                adasdasdasdasdasda
-                asdasdasdasadasdasdas
-            </p>
-            <p>adasdasdasdasdasdaasdasdas
-            
-            </p>
+            <p>After working in Tourism for years I decided to do a career change and start this amazing adventure at Ironhack.</p>
+            <p>Something Nash and I have in common is that we both live currently in Germany and try to find an opportunity as Web Developers, that is how we came to the idea of creating Ready2Work, a place for people like us in search of our dream job.</p>
+            <p><img className='githubIcon' alt="github" src={GitHub}></img>github.com/LGG93</p>
+            <p><img className='githubIcon' alt="github" src={LinkedIn}></img>www.linkedin.com/in/lorena-garcia-galan</p>
          </div>   
         </div>
     </div>
